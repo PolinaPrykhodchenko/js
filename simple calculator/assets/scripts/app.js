415b09c5bde8e77c19d1c9f686c9a2a9ce3616a1
polyasik <polina.prykhodchenko@gmail.com>
2024-07-16 17:02:14 +0300

    added calculation logic for all btn

--- simple calculator/assets/scripts/app.js
+++ simple calculator/assets/scripts/app.js
@@ -1,5 +1,52 @@
-alert('This works!');
-
 let resultOfCalculation = 0;
 
-outputResult(resultOfCalculation, '');
+function getUserInput() {
+    return  parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCalculation, userInput) {
+    const calcDescription = `${resultBeforeCalculation} ${operator} ${userInput}`;
+
+    outputResult(resultOfCalculation, calcDescription);
+}
+
+function add() {
+    const userInput = getUserInput();
+    const resultBeforeCalculation = resultOfCalculation;
+
+    resultOfCalculation = resultOfCalculation + userInput;
+
+    createAndWriteOutput('+', resultBeforeCalculation, userInput);
+}
+
+function subtract() {
+    const userInput = getUserInput();
+    const resultBeforeCalculation = resultOfCalculation;
+
+    resultOfCalculation = resultOfCalculation - userInput;
+
+    createAndWriteOutput('-', resultBeforeCalculation, userInput);
+}
+
+function multiply() {
+    const userInput = getUserInput();
+    const resultBeforeCalculation = resultOfCalculation;
+
+    resultOfCalculation = resultOfCalculation * userInput;
+
+    createAndWriteOutput('*', resultBeforeCalculation, userInput);
+}
+
+function divide() {
+    const userInput = getUserInput();
+    const resultBeforeCalculation = resultOfCalculation;
+
+    resultOfCalculation = resultOfCalculation / userInput;
+
+    createAndWriteOutput('/', resultBeforeCalculation, userInput);
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
